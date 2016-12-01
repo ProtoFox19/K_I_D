c693aca9b9bf3addb613ae7830c88fa95efd1917
ProtoFox19 <proto.fox@web.de>
2016-12-01 03:22:47 +0100

    Menue_und_Pausenmenue_hinzugefuegt

---
+++ K_I_D_/Assets/Scripts/MainMenue.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenue : MonoBehaviour {
+
+    //Die Panels die jeweils sichtbar und unsichtbar gemacht werden sollen
+    public GameObject Hauptmenue, Levelmenue, Optionenmenue, Impressummenue, Level1, Level2, Level3;
+
+    //Wenn auf Level geklickt wurde
+    public void OnLevel(bool clicked)
+    {
+        //SceneManager.LoadScene("Level2");
+        if (clicked)
+        {
+            Levelmenue.SetActive(clicked);
+            Hauptmenue.SetActive(!clicked);
+            Optionenmenue.SetActive(!clicked);
+            Impressummenue.SetActive(!clicked);
+        }
+        else
+        {
+            Levelmenue.SetActive(clicked);
+            Hauptmenue.SetActive(!clicked);
+            Optionenmenue.SetActive(clicked);
+            Impressummenue.SetActive(clicked);
+        }
+    }
+
+    //Wenn auf Obtionen geklickt wurde
+    public void OnOptionen(bool clicked)
+    {
+        if (clicked)
+        {
+            Optionenmenue.SetActive(clicked);        //wird das OptionsmenuePanel sichtbar gemacht
+            Hauptmenue.SetActive(!clicked);          //wird das HauptmenuePanel unsichtbar gemacht
+            Impressummenue.SetActive(!clicked);
+            Levelmenue.SetActive(!clicked);
+        }
+        else
+        {
+            Optionenmenue.SetActive(clicked);
+            Hauptmenue.SetActive(!clicked);
+            Impressummenue.SetActive(clicked);
+            Levelmenue.SetActive(clicked);
+        }
+    }
+
+    //Wenn auf Impressum geklickt wurde
+    public void OnImpressum(bool clicked)
+    {
+        if (clicked)
+        {
+            Impressummenue.SetActive(clicked);
+            Hauptmenue.SetActive(!clicked);
+            Optionenmenue.SetActive(!clicked);
+            Levelmenue.SetActive(!clicked);
+        }
+        else
+        {
+            Impressummenue.SetActive(clicked);
+            Hauptmenue.SetActive(!clicked);
+            Optionenmenue.SetActive(clicked);
+            Levelmenue.SetActive(clicked);
+        }
+    }
+
+    public void OnVerlassen()
+    {
+        Application.Quit();
+    }
+
+
+    /*Level Buttons*/
+
+    public void OnLevel1()
+    {
+
+    }
+
+    public void OnLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void OnLevel3()
+    {
+
+    }
+}
---
+++ K_I_D_/Assets/Scripts/PausMenue.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PausMenue : MonoBehaviour
+{
+
+    // Objekte die Versteckt werden muessen, oder sichtbar gemacht werden muessen
+    public GameObject Pauseb, Weissesoverlay, Roterrand, Weiterspielen, Neustart, Hauptmenue;
+
+    //Am Anfang soll der zustand des nicht gedrueckten Pausenbuttons herschen
+    public void Start()
+    {
+        OnWeiterspielenButton();
+    }
+
+    //Wenn auf den Pausebutton gedrueckt wurde
+    public void OnPauseButton()
+    {
+        Weissesoverlay.SetActive(true);     //Mach das Overlay sichtbar
+        Roterrand.SetActive(true);
+        Pauseb.SetActive(false);            //mach den Pausebutton unsichtbar
+        Time.timeScale = 0;                 //halt das Spiel an
+    }
+
+    //Wenn auf den Weiterspielen Button gedrueckt wurde
+    public void OnWeiterspielenButton()
+    {
+        Weissesoverlay.SetActive(false);
+        Roterrand.SetActive(false);
+        Pauseb.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    //Fuer ein neustart laed er die Szene einfach neu
+    public void OnNeustartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //Das Hauptmenue heisst als Szene Start
+    public void OnHauptmenue()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
