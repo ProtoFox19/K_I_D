4833f997a4f07d19e131f9f3969dc0c50ccb41d6
ProtoFox19 <proto.fox@web.de>
2017-01-08 22:02:21 +0100

    LoadingScene ist nun vorhanden
    Loading Scene funktioniert nur nicht sooo gut auf dem Handy wieder mal
    ...

---
+++ K_I_D_/Assets/Scripts/LoadTargetSceneButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadTargetSceneButton : MonoBehaviour {
+
+	public void LoadSceneNum(int num) {
+        if(num < 0 || num >= SceneManager.sceneCountInBuildSettings) {
+            Debug.LogWarning("Kann die Scene Nummer" + num + "nicht laden, SceneManager hat nur" + SceneManager.sceneCountInBuildSettings + "scenen in den BuildSettings");
+            return;
+        }
+        Time.timeScale = 1;
+        LoadingScreenManager.LoadScene(num);
+
+    }
+}
