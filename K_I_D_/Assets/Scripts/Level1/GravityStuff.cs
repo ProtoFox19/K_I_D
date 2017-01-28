c7782ba7c0dbdcb03d51833cc2eca4c719466e9e
MaxFlux <rothmax91@gmail.com>
2017-01-28 02:00:25 +0100

    mit Beispiel Ausschmückung

---
+++ K_I_D_/Assets/Scripts/Level1/GravityStuff.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityStuff : MonoBehaviour {
+
+	// RigidBody erzeugen
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+		// RigidBody
+		rb = GetComponent<Rigidbody> ();
+
+	}
+
+	void OnTriggerEnter (Collider other) {
+
+
+		if (other.CompareTag("Player")) {
+
+			rb.useGravity = true;
+		}
+	}
+}
